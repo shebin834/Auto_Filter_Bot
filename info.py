fd945bb0ce2bd33faeb68f70313433d5a5043203
shebin834 <shebinkallankunnan@gmail.com>
2026-02-09 15:39:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'MalluTeaters_files') # Collect
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://shebin:shebin123@cluster1.wovxqek.mongodb.net/?appName=Cluster1")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://user:password@clustername.mongodb.net/dbname")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
 # ============================
 # Movie Notification & Update Settings
 # ============================
